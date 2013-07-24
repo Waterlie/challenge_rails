2b39a5060ab22fb07e6856149bd1cd91fd01f2ed
Alexandre LOMAN <alexandreloman@gmail.com>
2013-07-24 23:35:01 +0200

    title ok

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
 
-  def full_title(page_title)
+  def full_title
     base_title = "Challenge Rails"
-    if page_title.empty?
+    if @title.nil?
       base_title
     else
-      "#{base_title} | #{page_title}"
+      "#{base_title} | #{@title}"
     end
   end
 
